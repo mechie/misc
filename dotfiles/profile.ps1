f63d955b06c1fecee614fd3233b67e26d2d678ad
Guillem <gm@sal.cat>
2023-04-28 10:48:05 -0400

    Add GetCommandPath to posh dotfile

--- dotfiles/profile.ps1
+++ dotfiles/profile.ps1
@@ -3,3 +3,14 @@ function Prompt {
     Write-Host (";") -NoNewLine -ForegroundColor $(If (($LastExitCode -eq $null -or $LastExitCode -eq 0) -and ($?)) {"Green"} else {"Red"})
     return " "
 }
+
+function GetCommandPath([string]$Command) {
+    try {
+        (Get-Command "${Command}").Path;
+    }
+    catch {
+        Write-Error("Could not Get-Command. It may not be reachable, or your shell is misbehaving");
+    }
+}
+
+Set-Alias -Name which -Value GetCommandPath
